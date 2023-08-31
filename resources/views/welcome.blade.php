3ae0c3230960827e8da17910a02435dc3c99ac37
MunyinyaMaurice <munyinya13@gmail.com>
2023-08-31 23:52:42 +0200

    interface impl

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,18 @@
         </form>
     </div>
 
-
+    <div> 
+        <h3>Buy Movie Tickets N500.00</h3>
+    <form method="POST" action="{{ route('pay') }}" id="paymentForm">
+        {{ csrf_field() }}
+    
+        <input name="name" placeholder="Name" />
+        <input name="email" type="email" placeholder="Your Email" />
+        <input name="phone" type="tel" placeholder="Phone number" />
+    
+        <input type="submit" value="Buy" />
+    </form>
+    </div>
     
 </body>
 </html>
